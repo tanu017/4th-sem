e17a6cd9656db755542a7dbc19c47a46000eefae
Tanushree <122681103+tanu017@users.noreply.github.com>
2025-04-01 21:59:38 +0530

    Create program10.py

---
+++ program10.py
@@ -0,0 +1,29 @@
+MAX_CHARS = 256
+def max(a, b):
+  return a if a>b else b
+
+def badCharHeuristic(pat, size, badchar):
+  for i in range(MAX_CHARS):
+    badchar[i] = -1
+  for i in range(size):
+    badchar[ord(pat[i])] = i
+
+def patternsearch(text, pat):
+  m = len(pat)
+  n = len(text)
+  badchar = [-1] * MAX_CHARS
+  badCharHeuristic(pat, m, badchar)
+  s = 0
+  while s <= (n-m):
+    j = m-1
+    while j>=0 and pat[j] == text[s+j]:
+      j -= 1
+    if j < 0:
+      print("\nPattern occurs at position =", s)
+      s += m - badchar[ord(text[s+m])] if (s+m) < n else 1
+    else: 
+      s += max(1, j-badchar[ord(text[s+j])])
+
+text= input("Enter the text: ").rstrip('\n')
+pat= input("Enter the pattern: ").rstrip('\n')
+patternsearch(text, pat)
